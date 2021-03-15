a0130433970c2b68147afcb5b254ba3b756d6e63
Syper_olao <olay1234538@gmail.com>
2021-03-15 22:37:16 +0300

    Добавлены метод расчета значения строки

--- src/calc/com/calculatons/Calculations.java
+++ src/calc/com/calculatons/Calculations.java
@@ -28,9 +28,55 @@ public class Calculations {
         }
     }
 
-    //Метод возвращает true, если проверяемый символ - разделитель ("пробел" или "равно")
+    //Метод возвращает true, если проверяемый символ - разделитель
     static private boolean isSeparator(char symbol) {
         return ((" =".indexOf(symbol) != -1));
     }
+
+
+    static private double Counting(String input) {
+        var arrInput = input.toCharArray();
+        Stack<Double> temp = new Stack<>();
+
+        for (int i = 0; i < input.length(); i++)
+        {
+            //Если символ - цифра, то читаем все число и записываем на вершину стека
+            if (Character.isDigit(arrInput[i])) {
+                String a = "";
+                while (!isSeparator(arrInput[i]) && !isOperator(arrInput[i]))
+                {
+                    a += arrInput[i];
+                    i++;
+                    if (i == arrInput.length) break;
+                }
+                temp.push(Double.parseDouble(a));
+                i--;
+            } else if (isOperator(arrInput[i]))
+            {
+                double second = temp.pop();
+                double first = temp.pop();
+                //Вычисляется результат
+                temp.push(tempResCalc(second, first, arrInput[i]));
+            }
+        }
+        return temp.peek();
+    }
+
+    private static double tempResCalc(double second, double first, char symbol) {
+        switch (symbol) {
+            case '+':
+                return first + second;
+            case '-':
+                return first - second;
+            case '*':
+                return first * second;
+            case '/':
+                return first / second;
+            case '^':
+                return Double.parseDouble(String.valueOf(Math.pow(first, second)));
+        }
+        return 0;
+    }
 }
+
 
